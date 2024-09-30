4828747b3ce0cbc6aaee2cd5cd4754aee3eef490
Huda-slo <huda1slo31@yahoo.com>
2024-09-30 09:14:04 +0300

    Initial Commit

---
+++ App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Huda Almadi on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
