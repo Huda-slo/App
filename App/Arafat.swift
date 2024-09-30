1ea0c780889616ba8773433b1991d5e820dee5df
Arafat-Alsaieq <a.h.b.alsaieq@gmail.com>
2024-09-30 10:21:24 +0300

    update

---
+++ App/Arafat.swift
@@ -0,0 +1,18 @@
+//
+//  Arafat.swift
+//  App
+//
+//  Created by Arafat on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Arafat: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Arafat()
+}
