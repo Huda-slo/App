3bce5b47b8818a7f53e4c87e43384f0e732e69f6
Reema-M0 <Reemaal200200@gmil.com>
2024-10-01 02:53:21 -0700

    RM2

--- App/Profile page2.swift
+++ App/Profile page2.swift
@@ -9,12 +9,46 @@ import SwiftUI
 
 struct Profile_page2: View {
     var body: some View {
-        ZStack { Color.background .ignoresSafeArea()}
+        
+            VStack{
+                HStack{
+                    Text("تعديل الملف الشخصي")
+                    
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .foregroundColor(.background)
+                        .multilineTextAlignment(.trailing)
+                    .frame(width:300 , height: 100)}
+            
+                
+                VStack {
+                    
+                    
+                    Image("ImageProfile")
+                        .resizable()
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        .frame(width: 100, height: 100)
+                        .overlay(Circle().stroke(Color.background,
+                                                 lineWidth: 4))
 
+                    
+                        Text("الإسم")
+                            .font(/*@START_MENU_TOKEN@*/.title3/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.background)
+                            .multilineTextAlignment(.trailing)
+                        .padding(.leading, 250)
+                    
+                    TextField("اسم الاعب ", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        .padding(.leading, 280)
 
+                    Spacer()
+                   
+                }
+            }
+        }
     }
-}
 
-#Preview {
-    Profile_page2()
-}
+    
+        #Preview {
+            Profile_page2()
+        }
