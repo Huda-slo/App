bd08140c77b788ec041562f8d74390c57edab59d
Huda-slo <huda1slo31@yahoo.com>
2024-10-01 09:40:00 +0300

    Huda

---
+++ App/Profile page1.swift
@@ -0,0 +1,18 @@
+//
+//  Profile page1.swift
+//  App
+//
+//  Created by Huda Almadi on 28/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Profile_page1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Profile_page1()
+}
--- App/Totorial Page1.swift
+++ App/Totorial Page1.swift
@@ -9,35 +9,62 @@ import SwiftUI
 
 struct Totorial_Page1: View {
     var body: some View {
+        
+        ZStack {
+            Color.background
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            
+            
+            VStack {
+                Image( "Tutotial1")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Spacer()
                 
-                VStack {
-                    Image( "Tutotial1")
-                        .imageScale(.large)
-                        .foregroundStyle(.tint)
+                
+                VStack(alignment: .leading) {
+                    Text("مرحبا بك في")
+                        .font(.title)
+                        .foregroundColor(.orange1)
+                    
+                    Text("اسمعك")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.orange1)
                     Spacer()
                     
                     
-                    VStack(alignment: .leading) {
-                        Text("مرحبا بك في")
-                                .font(.title)
-                                .foregroundColor(.orange1)
-                        
-                        Text("اسمعك")
-                            .font(.title)
-                            .fontWeight(.bold)
-                            .foregroundColor(.orange1)
-                        Spacer()
-                        
-                        
-                        
-                    }
-                    
-                }
                 
                     
+                    VStack {
+                        Spacer()
+                            .frame(height: 88)
+                        
+                        HStack {
+                            Spacer()
+                            
+                            Image("Button-next")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                            
+                            
+                            
+                            
+                            
+                            
+                        }
+                        
+                    }
+                }
+                
             }
+            
+            
         }
-
+    }
+}
        
 #Preview {
     Totorial_Page1()
