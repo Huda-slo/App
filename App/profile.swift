7dcab07d770a0eda8baa153c476772650f626586
BayanADA <Bayanalshuwaier@gmail.com>
2024-10-01 12:02:22 +0300

    font and slider

--- App/profile.swift
+++ App/profile.swift
@@ -6,13 +6,31 @@
 //
 
 import SwiftUI
+struct FontSizeKey: EnvironmentKey { static let defaultValue: CGFloat = 14}
+
 
 struct profile: View {
     @State private var volume: Double = 0
+    @State private var fontSize: Int = 14
     var body: some View {
+        Spacer()
+        Text("حجم الخط")
+            .font(.system(size: CGFloat(fontSize)))
+        Picker(selection: $fontSize, label: /*@START_MENU_TOKEN@*/Text("Picker")/*@END_MENU_TOKEN@*/) {
+            Text("صغير").tag(12)
+            Text("وسط").tag(14)
+            Text("كبير").tag(15)
+            Text("كبير جدًا").tag(16)
+                .pickerStyle(SegmentedPickerStyle())
+        }
+        
+        Spacer()
         Text("مستوى الصوت")
         Slider(value: $volume, in: 0...100)
+            .frame(width: 250, height: 30)
             .accentColor(.orange1)
+        Spacer()
+            
             
             
             
