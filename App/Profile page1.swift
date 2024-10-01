8713b3d00176c2ccffdcd28dd6f632af0f5ef08a
Huda-slo <huda1slo31@yahoo.com>
2024-10-01 12:57:46 +0300

    huda

--- App/Profile page1.swift
+++ App/Profile page1.swift
@@ -1,48 +1,165 @@
 //
-//  Profile page1.swift
-//  App
+//  ContentView.swift
+//  HudaTest
 //
 //  Created by Huda Almadi on 28/03/1446 AH.
 //
 
 import SwiftUI
 
-struct Profile_page1: View {
+struct Profilepage1: View {
     var body: some View {
-        
-        ZStack {
-            Color.orange2
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack {
-                Text("الملف الشخصي")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding(.trailing, 20)
+        VStack {
+    
+            ZStack{
+                RoundedRectangle(cornerRadius: 50, style: .continuous)
+                    .fill(Color.orange2)
+                    .frame(height: 350)
                 
-                
-                Image( "profile pic")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 150, height: 150.0)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.white, lineWidth: 2))
-                    .shadow(radius: 10)
-                    .padding(.top, 10)
-                
-                Spacer()
-                
-                
+                    .overlay(
+                        VStack(spacing: 20) {
+                            Text("الملف الشخصي")
+                                .font(.largeTitle)
+                                .foregroundColor(.white)
+                            
+                            
+                            
+                            Image("profile pic")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 100, height: 100)
+                                .clipShape(Circle())
+                                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                            
+                            
+                            Text("اسم اللاعب")
+                                .font(.title2)
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                            
+                            Text("الهدف")
+                                .font(.caption)
+                                .foregroundColor(.white)
+                                .padding(.bottom, 5)
+                            
+                            Text("Hi players, I'm from Los Angeles and I love playing soccer and car racing games with fellow players. Let's connect!")
+                                .font(.footnote)
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(.white)
+                                .padding(.horizontal)
+                        
+                            
+                        }
+                    )
+                  
             }
             
-           
+            HStack(spacing: 40) {
+                VStack {
+                                    Text("المستوى")
+                                        .font(.title3)
+                                        .foregroundColor(.orange)
+                                    Text("10")
+                                        .font(.title)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.yellow)
+                                }
+                
+                VStack {
+                                    Text("الاشتراك")
+                                        .font(.title3)
+                                        .foregroundColor(.orange)
+                                    Text("98")
+                                        .font(.title)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.yellow)
+                                }
+                VStack {
+                                    Text("النقاط")
+                                        .font(.title3)
+                                        .foregroundColor(.orange)
+                                    Text("67")
+                                        .font(.title)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.yellow)
+                                }
+                
+            }
+            .padding(.top, 10)
+            
+            Spacer()
+            
+            HStack(spacing: 40) {
+                VStack {
+                    Image("fire")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(.orange)
+                    
+                    Text("ستريك")
+                        .font(.title3)
+                        .foregroundColor(.orange)
+                    
+                    Text("4")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.yellow)
+                    
+                }
+                
+                VStack {
+                    Image("read")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(.orange)
+                    
+                    Text("المستوى")
+                        .font(.title3)
+                        .foregroundColor(.orange)
+                                        
+
+                    Text("2")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.yellow)
+                    
+                    
+                }
+                
+                VStack {
+                    Image("success")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40, height: 40)
+                        .foregroundColor(.orange)
+                                            
+                    Text("المرحلة")
+                        .font(.title3)
+                        .foregroundColor(.orange)
+                                            
+                    Text("1")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.yellow)
+                    
+                }
+                
+                        }
+            .padding(.top, 10)
+            
+            Spacer()
+            
             
         }
         
-            
+        
+        .padding()
     }
 }
 
+
 #Preview {
-    Profile_page1()
+    Profilepage1()
 }
