433432b2b342a8dda25ace6382fe8ee3c384accf
Huda-slo <huda1slo31@yahoo.com>
2024-10-01 10:44:33 +0300

    huda

--- App/Profile page1.swift
+++ App/Profile page1.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct Profile_page1: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            Color.orange2
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("الملف الشخصي")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding(.trailing, 20)
+                
+                
+                Image( "profile pic")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 150, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                    .shadow(radius: 10)
+                    .padding(.top, 10)
+                
+                Spacer()
+                
+                
+            }
+            
+           
+            
+        }
+        
+            
     }
 }
 
