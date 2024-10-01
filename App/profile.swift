31320399c39ea2467afd0524e47d9d759fa594c4
BayanADA <Bayanalshuwaier@gmail.com>
2024-10-01 10:24:29 +0300

    ..

---
+++ App/profile.swift
@@ -0,0 +1,22 @@
+//
+//  profile.swift
+//  App
+//
+//  Created by Bayan Alshuwaier on 01/10/2024.
+//
+
+import SwiftUI
+
+struct profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Slider(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(10)/*@END_MENU_TOKEN@*/)
+            
+            
+            
+    }
+}
+
+#Preview {
+    profile()
+}
