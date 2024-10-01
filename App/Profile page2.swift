675c787b70e85d3a29983de6ad230bb28b20e7b5
Reema-M0 <Reemaal200200@gmil.com>
2024-10-01 00:45:23 -0700

    RM

---
+++ App/Profile page2.swift
@@ -0,0 +1,20 @@
+//
+//  Profile page2.swift
+//  App
+//
+//  Created by Reem on 28/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Profile_page2: View {
+    var body: some View {
+        ZStack { Color.background .ignoresSafeArea()}
+
+
+    }
+}
+
+#Preview {
+    Profile_page2()
+}
