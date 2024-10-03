2e61b5d68bf74baa9a5a6b2ef1494d5c910870e5
Huda-slo <huda1slo31@yahoo.com>
2024-10-03 10:48:44 +0300

    huda's commit

--- App/landing2.swift
+++ App/landing2.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct Arafat: View {
+struct landing2: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    Arafat()
+    landing2()
 }
