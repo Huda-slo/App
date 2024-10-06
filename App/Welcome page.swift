5aafb87c60b47ca6a421932431ffc289566f8d8b
Reema-M0 <Reemaal200200@gmil.com>
2024-10-06 00:01:38 -0700

    rr

--- App/Welcome page.swift
+++ App/Welcome page.swift
@@ -8,67 +8,66 @@
 import SwiftUI
 
 struct Welcome_Page: View {
+    @State private var moveLogo1 = false
+    @State private var moveLogo2 = false
+    
     var body: some View {
         
         ZStack {
             LinearGradient(gradient: Gradient(colors: [Color("orange1"), Color("CustomOrangeBottom")]), startPoint: .top, endPoint: .bottom)
-                            .edgesIgnoringSafeArea(.all)
+                .edgesIgnoringSafeArea(.all)
             
             VStack {
                 Spacer()
+                
                 ZStack {
                     Image("logo1")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 150, height: 150)
+                        .frame(width: 200, height: 150)
+                        .offset(x: -15, y: moveLogo1 ? -20 : 4)  
+
+                        .animation(.easeInOut(duration: 1).repeatForever(autoreverses: true), value: moveLogo1)
+                        .onAppear {
+                            moveLogo1.toggle()
+                        }
                     
+
                     Image("logo2")
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 75, height: 75)
-                        .offset(x: 50, y: -50)
-                              
+                        .frame(width: 100, height: 90)
+                        .offset(x: 90, y: moveLogo2 ? -70 : -10)  
+
+                        .animation(.easeInOut(duration: 1.5).repeatForever(autoreverses: true), value: moveLogo2)
+                        .onAppear {
+                            moveLogo2.toggle()
+                        }
                 }
                 .padding(.bottom, 20)
                 
-                
+
                 Text("اسمعك")
                     .font(.largeTitle)
                     .foregroundColor(.white)
                 
                 Spacer()
                 
-                
-                
+
                 HStack {
                     Image("Graphics")
                         .resizable()
                         .scaledToFill()
                         .frame(height: UIScreen.main.bounds.height * 0.35)
                         .clipped()
-                    
-    
-                    
                 }
                 .padding([.leading, .trailing], 20)
-                               .padding(.bottom, 30)
-                
-                
+                .padding(.bottom, 30)
             }
         }
-        
-        
-        
-        
-        
-        
-        
-        
-        
     }
-    
 }
-       
+
 #Preview {
     Welcome_Page()
 }
