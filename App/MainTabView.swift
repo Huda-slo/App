59c1ff9dbcbf40b99145e6df396518666be2e3e5
Reema-M0 <Reemaal200200@gmil.com>
2024-10-07 01:59:11 -0700

    ===

--- App/MainTabView.swift
+++ App/MainTabView.swift
@@ -1,11 +1,14 @@
-
 import SwiftUI
 
 struct MainTabView: View {
     var body: some View {
         TabView {
             // الصفحة الأولى (خريطة المستويات)
-           
+            Home1Map()
+                .tabItem {
+                    Image(systemName: "map.fill")  // أيقونة الصفحة الأولى
+                    Text("الخريطة")                // النص أسفل الأيقونة
+                }
             
             // الصفحة الثانية (الملف الشخصي)
             Profilepage1()
@@ -13,38 +16,17 @@ struct MainTabView: View {
                     Image(systemName: "person.fill")  // أيقونة الصفحة الثانية
                     Text("الملف الشخصي")             // النص أسفل الأيقونة
                 }
-            Home1Map()
-                .tabItem {
-                    Image(systemName: "map.fill")  // أيقونة الصفحة الأولى
-                    Text("الخريطة")                // النص أسفل الأيقونة
-                }
             
-            // الصفحة الثالثة (الإعدادات)
-            profile()
+            // الصفحة الثالثة (المراجع)
+            ReferencesPage() // استبدال الصفحة بالإعدادات
                 .tabItem {
-                    Image(systemName: "gearshape.fill")  // أيقونة الصفحة الثالثة
-                    Text("الإعدادات")
-                    
-                    //
-                    //  profile.swift
-                    //  App
-                    //
-                    //  Created by Bayan Alshuwaier on 01/10/2024.
-                    //
-
-
-
-// النص أسفل الأيقونة
+                    Image(systemName: "book.fill")  // أيقونة المراجع
+                    Text("المراجع")                // النص أسفل الأيقونة
                 }
         }
         .accentColor(.orange) // لون الأيقونات عند اختيار التبويب
     }
 }
-
-#Preview {
-    MainTabView()
-}
-
 
 #Preview {
     MainTabView()
